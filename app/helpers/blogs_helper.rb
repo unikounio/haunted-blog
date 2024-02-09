cd94d32f90840be51a582d59f97a51a0c64d181e
unikounio <bubbbble428@gmail.com>
2024-02-09 21:47:49 +0900

    脆弱性排除のためhtml_safeを削除した

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -2,6 +2,6 @@
 
 module BlogsHelper
   def format_content(blog)
-    h(blog.content).gsub("\n", '<br>').html_safe # rubocop:disable Rails/OutputSafety
+    h(blog.content).gsub("\n", '<br>')
   end
 end
